1c44d83c08b2b3c8d35bd86d88a12ca20414e5e1
Alex Seifert <alex@alexseifert.com>
2023-09-13 11:40:10 +0200

    Added a dynamic main menu

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
 <?php
-  // Get the full URI and break it apart
-  $full_path = rtrim($_SERVER["REQUEST_URI"], "/");
-  $path_parts = explode("/", $full_path);
+  function parse_uri() {
+    // Get the full URI and break it apart
+    $full_path = rtrim($_SERVER["REQUEST_URI"], "/");
+    $path_parts = explode("/", $full_path);
+  
+    // Remove the unneeded parts: "/" and "index.php"
+    $amount_to_remove = isset($path_parts[1]) && $path_parts[1] === "index.php" ? 2 : 1;
+    array_splice($path_parts, 0, $amount_to_remove);
 
-  // Remove the unneeded parts: "/" and "index.php"
-  $amount_to_remove = isset($path_parts[1]) && $path_parts[1] === "index.php" ? 2 : 1;
-  array_splice($path_parts, 0, $amount_to_remove);
+    return $path_parts;
+  }
+
+  function parse_uri_string(): string {
+    $path_parts = parse_uri();
+    return implode("/", $path_parts);
+  }
+
+  $path_parts = parse_uri();
 
   $pages_config = array(
-    "home" => array(),
+    "home" => array(
+      "inMainNav" => true,
+      "mainNavTitle" => "Home"
+    ),
     "test" => array(
       "title" => "Test Page 1",
       "scripts" => ["testscripts.js"],
-      "styles" => ["teststyles.css"]
+      "styles" => ["teststyles.css"],
+      "inMainNav" => true,
+      "mainNavTitle" => "Test Page 1"
     ),
     "deeper/test" => array(
       "title" => "Test Page 2",
       "scripts" => ["testscripts.js"],
-      "styles" => ["teststyles.css"]
+      "styles" => ["teststyles.css"],
+      "inMainNav" => true,
+      "mainNavTitle" => "Test Page 2"
     ),
     "404" => array(
       "title" => "Page Not Found"
@@ -54,9 +72,18 @@
       <h1>Simple Frameworkless Website</h1>
 
       <nav class="main-nav">
-        <a href="/">Home</a>
-        <a href="/test">Test Page 1</a>
-        <a href="/deeper/test">Test Page 2</a>
+        <?php
+          foreach ($pages_config as $key => $_page_config) {
+            if (isset($_page_config["inMainNav"]) && $_page_config["inMainNav"] === true) {
+              if (parse_uri_string() === $key) {
+                echo "<a href=\"/" . $key . "\" class=\"selected-link\">" . $_page_config["mainNavTitle"] . "</a>";
+              }
+              else {
+                echo "<a href=\"/" . $key . "\">" . $_page_config["mainNavTitle"] . "</a>";
+              }
+            }
+          }
+        ?>
       </nav>
     </header>
 
