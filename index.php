db2306c9939e2d519c9c7fc9037c928d0519129a
Alex Seifert <alex@alexseifert.com>
2023-09-13 11:40:56 +0200

    Reorganized the code

--- index.php
+++ index.php
@@ -1,56 +1,55 @@
 <?php
-  function parse_uri() {
-    // Get the full URI and break it apart
-    $full_path = rtrim($_SERVER["REQUEST_URI"], "/");
-    $path_parts = explode("/", $full_path);
-  
-    // Remove the unneeded parts: "/" and "index.php"
-    $amount_to_remove = isset($path_parts[1]) && $path_parts[1] === "index.php" ? 2 : 1;
-    array_splice($path_parts, 0, $amount_to_remove);
+$pages_config = array(
+  "home" => array(
+    "inMainNav" => true,
+    "mainNavTitle" => "Home"
+  ),
+  "test" => array(
+    "title" => "Test Page 1",
+    "scripts" => ["testscripts.js"],
+    "styles" => ["teststyles.css"],
+    "inMainNav" => true,
+    "mainNavTitle" => "Test Page 1"
+  ),
+  "deeper/test" => array(
+    "title" => "Test Page 2",
+    "scripts" => ["testscripts.js"],
+    "styles" => ["teststyles.css"],
+    "inMainNav" => true,
+    "mainNavTitle" => "Test Page 2"
+  ),
+  "404" => array(
+    "title" => "Page Not Found"
+  )
+);
 
-    return $path_parts;
-  }
+function parse_uri() {
+  // Get the full URI and break it apart
+  $full_path = rtrim($_SERVER["REQUEST_URI"], "/");
+  $path_parts = explode("/", $full_path);
 
-  function parse_uri_string(): string {
-    $path_parts = parse_uri();
-    return implode("/", $path_parts);
-  }
+  // Remove the unneeded parts: "/" and "index.php"
+  $amount_to_remove = isset($path_parts[1]) && $path_parts[1] === "index.php" ? 2 : 1;
+  array_splice($path_parts, 0, $amount_to_remove);
 
+  return $path_parts;
+}
+
+function parse_uri_string(): string {
   $path_parts = parse_uri();
+  return implode("/", $path_parts);
+}
 
-  $pages_config = array(
-    "home" => array(
-      "inMainNav" => true,
-      "mainNavTitle" => "Home"
-    ),
-    "test" => array(
-      "title" => "Test Page 1",
-      "scripts" => ["testscripts.js"],
-      "styles" => ["teststyles.css"],
-      "inMainNav" => true,
-      "mainNavTitle" => "Test Page 1"
-    ),
-    "deeper/test" => array(
-      "title" => "Test Page 2",
-      "scripts" => ["testscripts.js"],
-      "styles" => ["teststyles.css"],
-      "inMainNav" => true,
-      "mainNavTitle" => "Test Page 2"
-    ),
-    "404" => array(
-      "title" => "Page Not Found"
-    )
-  );
+$path_parts = parse_uri();
+$page_key = count($path_parts) === 0 || $path_parts[0] === "" ? "home" : implode("/", $path_parts);
+$page_path = "pages/" . $page_key . ".php";
 
-  $page_key = count($path_parts) === 0 || $path_parts[0] === "" ? "home" : implode("/", $path_parts);
-  $page_path = "pages/" . $page_key . ".php";
-  
-  if (!file_exists($page_path)) {
-    $page_key = "404";
-    $page_path = "pages/404.php";
-  }
-  
-  $page_config = $pages_config[$page_key];
+if (!file_exists($page_path)) {
+  $page_key = "404";
+  $page_path = "pages/404.php";
+}
+
+$page_config = $pages_config[$page_key];
 ?>
 
 <html>
@@ -73,16 +72,16 @@
 
       <nav class="main-nav">
         <?php
-          foreach ($pages_config as $key => $_page_config) {
-            if (isset($_page_config["inMainNav"]) && $_page_config["inMainNav"] === true) {
-              if (parse_uri_string() === $key) {
-                echo "<a href=\"/" . $key . "\" class=\"selected-link\">" . $_page_config["mainNavTitle"] . "</a>";
-              }
-              else {
-                echo "<a href=\"/" . $key . "\">" . $_page_config["mainNavTitle"] . "</a>";
-              }
+        foreach ($pages_config as $key => $_page_config) {
+          if (isset($_page_config["inMainNav"]) && $_page_config["inMainNav"] === true) {
+            if (parse_uri_string() === $key) {
+              echo "<a href=\"/" . $key . "\" class=\"selected-link\">" . $_page_config["mainNavTitle"] . "</a>";
+            }
+            else {
+              echo "<a href=\"/" . $key . "\">" . $_page_config["mainNavTitle"] . "</a>";
             }
           }
+        }
         ?>
       </nav>
     </header>
@@ -96,11 +95,11 @@
     <script src="/assets/js/scripts.js"></script>
     
     <?php 
-      if (isset($page_config["scripts"])) {
-        foreach ($page_config["scripts"] as $script) {
-          echo "<script src=\"/assets/js/" . $script . "\"></script>";
-        }
+    if (isset($page_config["scripts"])) {
+      foreach ($page_config["scripts"] as $script) {
+        echo "<script src=\"/assets/js/" . $script . "\"></script>";
       }
+    }
     ?>
   </body>
 </html>
