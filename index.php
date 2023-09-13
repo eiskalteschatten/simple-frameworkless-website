a9dcabe4821ce303f3e7a44a84aa1adebdef1766
Alex Seifert <alex@alexseifert.com>
2023-09-13 09:55:58 +0200

    Added styles and code to determine the page from the URI

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php
+  // Get the full URI
+  $full_path = $_SERVER["REQUEST_URI"];
+  $path_parts = explode("/", $full_path);
 
-
+  // Remove the unneeded parts: "/" and "index.php"
+  array_splice($path_parts, 0, 2);
 ?>
 
 <html>
   <head>
     <title>Simple Frameworkless Website</title>
+
+    <link rel="stylesheet" href="/styles.css">
   </head>
   <body>
     <header>
       <h1>Simple Frameworkless Website</h1>
     </header>
 
-    <?php // pages come here ?>
+    <?php 
+      if (count($path_parts) === 0 || $path_parts[0] === "") {
+        echo "home!";
+      }
+      else {
+        echo "not home!";
+        // TODO: import the page based on the path (the directory structure should match the URI path)
+      }
+    ?>
 
     <footer>
       &copy; No One 2023
