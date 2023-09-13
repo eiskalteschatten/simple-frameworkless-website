8694c9f3beb0cc2ec81f54200eb91b121f7aae32
Alex Seifert <alex@alexseifert.com>
2023-09-13 10:27:42 +0200

    Added the page config

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
 <?php
-  // Get the full URI
-  $full_path = $_SERVER["REQUEST_URI"];
+  // Get the full URI and break it apart
+  $full_path = rtrim($_SERVER["REQUEST_URI"], "/");
   $path_parts = explode("/", $full_path);
 
   // Remove the unneeded parts: "/" and "index.php"
   array_splice($path_parts, 0, 2);
 
-  // TODO: page config:
-  // - path
-  // - title
-  // - etc
+  $pages_config = array(
+    "home" => array(),
+    "test" => array(
+      "title" => "Test Page 1"
+    ),
+    "deeper/test" => array(
+      "title" => "Test Page 2"
+    ),
+    "404" => array(
+      "title" => "Page Not Found"
+    )
+  );
+
+  $page_key = count($path_parts) === 0 || $path_parts[0] === "" ? "home" : implode("/", $path_parts);
+  $page_path = "pages/" . $page_key . ".php";
+  
+  if (!file_exists($page_path)) {
+    $page_key = "404";
+    $page_path = "pages/404.php";
+  }
+  
+  $page_config = $pages_config[$page_key];
 ?>
 
 <html>
   <head>
-    <title>Simple Frameworkless Website</title>
+    <title><?php if (isset($page_config["title"])) echo $page_config["title"] . " | "; ?> Simple Frameworkless Website</title>
 
     <link rel="stylesheet" href="/styles.css">
   </head>
@@ -23,16 +41,7 @@
       <h1>Simple Frameworkless Website</h1>
     </header>
 
-    <?php 
-      if (count($path_parts) === 0 || $path_parts[0] === "") {
-        include_once("pages/home.php");
-      }
-      else {
-        $page_path = "pages/" . implode("/", $path_parts) . ".php";
-        $page_path = file_exists($page_path) ? $path_path : "pages/404.php";
-        include_once($page_path);
-      }
-    ?>
+    <?php include_once($page_path); ?>
 
     <footer>
       &copy; No One 2023
