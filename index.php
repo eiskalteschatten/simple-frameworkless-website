10ea858774408ba365529393039ded8d37604ab3
Alex Seifert <alex@alexseifert.com>
2023-09-13 09:41:05 +0200

    Start the index.php page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+<html>
+  <head>
+    <title>Simple Frameworkless Website</title>
+  </head>
+  <body>
+    <header>
+      <h1>Simple Frameworkless Website</h1>
+    </header>
+
+    <?php // pages come here ?>
+
+    <footer>
+      &copy; No One 2023
+    </footer>
+  </body>
+</html>
